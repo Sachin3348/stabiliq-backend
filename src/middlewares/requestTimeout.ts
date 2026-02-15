e28a8b422e1adc68203a51005941f1c2d2ede0ea
sachinmahto11 <sachin.mahto@polariscampus.com>
2026-02-15 17:54:44 +0530

    fixed typescript warning

--- src/middlewares/requestTimeout.ts
+++ src/middlewares/requestTimeout.ts
@@ -6,7 +6,7 @@ const DEFAULT_MS = 25000; // 25 seconds
  * Sends 504 if the request doesn't complete within ms. Use on routes that may hang (e.g. DB + email).
  */
 export function requestTimeout(ms: number = DEFAULT_MS) {
-  return (req: Request, res: Response, next: NextFunction): void => {
+  return (_req: Request, res: Response, next: NextFunction): void => {
     let responded = false;
     const timer = setTimeout(() => {
       if (responded) return;
